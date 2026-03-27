f6e8bbceb5d2c11a2d5e9cc283385c5572d47533
pakinporing <pakinporing@gmail.com>
2026-03-27 17:14:19 +0700

    asdawd

--- src/libs/stripe.ts
+++ src/libs/stripe.ts
@@ -1,5 +1,6 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  //   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+  'pk_test_51RsEudF0jN7bj9Y6Z3r8TkrXxO7B7zkdjAo9EEm38UrOYCtR0nyvTlK3BolI0oAGmny3v46I2zKfU8kyJYUTDqPp00EiFttpFh'
 );
